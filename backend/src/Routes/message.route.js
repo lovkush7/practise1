acf342eb39e25106c3e07bf9ca3ea6eb0fe5c7f0
lovkush7 <karkilov46@gmail.com>
2025-09-02 17:26:40 +0545

    frontend setup

--- backend/src/Routes/message.route.js
+++ backend/src/Routes/message.route.js
@@ -2,10 +2,14 @@ import { Router } from "express";
 import protectedroute from "../middleware/protected.js";
 import getuser from "../messagescontroller/getuser.js";
 import getmessages from "../messagescontroller/getmessages.js";
+import sendmessage from "../messagescontroller/send.message.js";
 
 const router = Router();
 
 router.get("/user",protectedroute,getuser);
+
 router.get("/:id",protectedroute,getmessages);
+
+router.get("/send/:id",protectedroute,sendmessage)
 
 export default router;
---
+++ backend/src/messagescontroller/send.message.js
@@ -0,0 +1,37 @@
+import cloudinary from "../lib/cloudnary.js";
+import messagemodel from "../Model/message.js";
+
+
+const sendmessage = async(res,req)=>{
+
+    try{
+        const {text , image }=req.body;
+        const {id:receiverId} = req.params;
+        const senderId = req.user._id;
+
+         let imageurl;
+
+        if(image){
+         const uploadresponse = await cloudinary.uploader.upload(image);
+         imageurl = uploadresponse.secure_url;
+        }
+
+        const newmessage = new messagemodel({
+            senderId,
+            receiverId,
+            text,
+            image:imageurl
+        });
+
+        await newmessage.save();
+
+        //we will do the socket io here
+
+        res.status(200).json(newmessage);
+
+    }catch(err){
+        console.error("the error is "+err)
+    }
+
+}
+export default sendmessage;
