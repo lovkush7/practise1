267bb1f464c91bc4a0a2f8516455bd89b72ead5a
lovkush7 <karkilov46@gmail.com>
2025-09-04 20:26:26 +0545

    small setup

--- froentend/src/pages/signup/signup.jsx
+++ froentend/src/pages/signup/signup.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import "./signup.css"
 import { LuMessageSquareDiff } from "react-icons/lu";
+import {Link, useNavigate} from "react-router-dom";
+import api from '../../api/axios';
 
 const Signup = () => {
   const [FormData, SetFormData] = useState({
@@ -8,8 +10,25 @@ const Signup = () => {
     email: "",
     password: ""
   })
-  const handlesubmit = (e) => {
+  const navigate = useNavigate()
+  const handlesubmit =async (e) => {
     e.preventDefault();
+    const response = await api.post("/auth/signup",{
+      ...FormData
+    });
+    console.log(response);
+    const result = await response.data;
+    console.log(result);
+
+    const {success,error}=result;
+    if(success){
+      setTimeout(() => {
+        navigate('/login')
+      }, 1000);
+    }
+    else if(error){
+      const details = error?.details[0].message;
+    }
   }
 
   const handleform = (e)=>{
@@ -21,6 +40,7 @@ const Signup = () => {
     <div style={{ display: "flex", minHeight: "100vh" }}>
       <div className='left-side' style={{ display: "flex", flexDirection: "column",width:"50%",maxWidth:"500px", justifyContent: "center", alignItems: "center", padding: "6px" }}>
         {/* <div className="left-mid"  style={{display:"flex",justifyContent:"center",alignItems:"center",flexDirection:"column"}}> */}
+        <div className='logo-box'>
           <div className="logo">
             <LuMessageSquareDiff className='logo-mess' />
           </div>
@@ -29,8 +49,8 @@ const Signup = () => {
             <p>get started with your account</p>
           </div>
 
-        {/* </div> */}
-        <form className='form' action="">
+        </div>
+        <form onSubmit={handlesubmit} className='form' action="">
           <div className="input">
             <label >Name</label>
             <input type="text" 
@@ -56,10 +76,15 @@ const Signup = () => {
             />
           </div>
           <div className="input" style={{}}>
-            <input type="submit" value={"Signup"} />
+            <input type="submit" value={"Signup"} onChange={handlesubmit} />
           </div>
       
         </form>
+        <div className="text" style={{display:"flex",justifyContent:'space-between',width:'100%'}}>
+          <p style={{}}>Already have an account ?
+            <Link to="/login">sign in</Link>
+          </p>
+        </div>
 
       </div>
       <div className="right-side" style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", padding: "6px" }}>
